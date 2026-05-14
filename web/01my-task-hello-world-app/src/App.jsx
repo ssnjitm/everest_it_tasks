d3f82655230fe3d28dad6f8c100a4676ef384069
Sanjeet Mijar <ssnjitm6@gmail.com>
2026-05-14 11:22:45 +0545

    hello world app

---
+++ web/01my-task-hello-world-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello World</h1>
+      <p>Welcome to my React Task Series!</p>
+    </div>
+  );
+}
+
+export default App;
